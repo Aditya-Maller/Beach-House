0cfef1a0e9757b67bbff43c7b43a421b65934dac
Aditya SM <adityamaller001@gmail.com>
2026-08-01 17:16:03 +0530

    Configure base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative pathing works on GitHub Pages subpaths
+  base: '/Beach-House/', // Ensures relative pathing works on GitHub Pages subpaths
   assetsInclude: ['**/*.PNG', '**/*.png', '**/*.jpg', '**/*.jpeg'],
   build: {
     outDir: 'dist',
